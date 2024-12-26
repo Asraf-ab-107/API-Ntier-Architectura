35ad7de32e4d31794e7462e42450c4041a921a25
Asraf-ab-107 <ashrafa-ab107@code.edu.az>
2024-12-26 11:30:52 +0400

    N-Tier arxitektura

---
+++ MyBlogApp.Core/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using MyBlogApp.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogApp.Core.Entities
+{
+    public class Category:BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ MyBlogApp.DAL/Context/BlogDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlogApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogApp.DAL.Context
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
